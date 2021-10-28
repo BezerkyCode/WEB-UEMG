e18bcbf7592cf0b00e778b104966b4fba6b8b60b
Gabriel Henrique da Silva <91507809+BezerkyCode@users.noreply.github.com>
2021-10-27 21:25:38 -0300

    Add files via upload

---
+++ Atividade 3 - JS/DataTypes.js
@@ -0,0 +1,10 @@
+//DATA TYPES JS
+
+//EXERCÍCIO 1
+
+let length = 16;         // Número 
+let lastName = "Johnson";// String 
+const x = {
+  firstName: "John",
+  lastName: "Doe"
+};                       // Objeto
